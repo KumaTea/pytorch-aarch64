8cf8b7a097907f2bdd5493614dfee4f9f96f8c0c
KumaTea <KumaTea@outlook.com>
2021-02-19 15:25:39 +0800

    split commands

--- build.sh
+++ build.sh
@@ -8,13 +8,15 @@ cd docker
 cd "py$PYVER"
 
 if [ "$PYVER" == "39" ]; then
-    docker build -f Dockerfile -t kumatea/pytorch:1.7.1 -t "kumatea/pytorch:1.7.1-py$PYVER" -t "kumatea/pytorch:$PYVER" .
+    docker build -t kumatea/pytorch:1.7.1 -t "kumatea/pytorch:1.7.1-py$PYVER" -t "kumatea/pytorch:$PYVER" .
 else
-    docker build -f Dockerfile -t "kumatea/pytorch:1.7.1-py$PYVER" -t "kumatea/pytorch:$PYVER" .
+    docker build -t "kumatea/pytorch:1.7.1-py$PYVER" -t "kumatea/pytorch:$PYVER" .
 fi
 
 if [ "$PYVER" == "39" ]; then
-    docker build -f full.Dockerfile -t kumatea/pytorch:latest kumatea/pytorch:1.7.1-full -t "kumatea/pytorch:1.7.1-py$PYVER-full" -t "kumatea/pytorch:$PYVER-full" .
-else
-    docker build -f full.Dockerfile -t "kumatea/pytorch:1.7.1-py$PYVER-full" -t "kumatea/pytorch:$PYVER-full" .
+    docker build -f full.Dockerfile -t kumatea/pytorch:latest .
+    docker build -f full.Dockerfile -t kumatea/pytorch:1.7.1-full .
 fi
+
+docker build -f full.Dockerfile -t "kumatea/pytorch:1.7.1-py$PYVER-full" .
+docker build -f full.Dockerfile -t "kumatea/pytorch:$PYVER-full" .
