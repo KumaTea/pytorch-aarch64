5197384f2d25b970cc367e356c65b50cce6afae7
KumaTea <KumaTea@outlook.com>
2021-02-19 15:13:46 +0800

    Modify arguments

--- build.sh
+++ build.sh
@@ -8,13 +8,13 @@ cd docker
 cd "py$PYVER"
 
 if [ "$PYVER" == "39" ]; then
-    docker build -t kumatea/pytorch:1.7.1 -t "kumatea/pytorch:1.7.1-py$PYVER" -t "kumatea/pytorch:$PYVER" -f Dockerfile .
+    docker build -f Dockerfile -t kumatea/pytorch:1.7.1 -t "kumatea/pytorch:1.7.1-py$PYVER" -t "kumatea/pytorch:$PYVER" .
 else
-    docker build -t "kumatea/pytorch:1.7.1-py$PYVER" -t "kumatea/pytorch:$PYVER" -f Dockerfile .
+    docker build -f Dockerfile -t "kumatea/pytorch:1.7.1-py$PYVER" -t "kumatea/pytorch:$PYVER" .
 fi
 
 if [ "$PYVER" == "39" ]; then
-    docker build -t kumatea/pytorch:latest kumatea/pytorch:1.7.1-full -t "kumatea/pytorch:1.7.1-py$PYVER-full" -t "kumatea/pytorch:$PYVER-full" -f full.Dockerfile .
+    docker build -f full.Dockerfile -t kumatea/pytorch:latest kumatea/pytorch:1.7.1-full -t "kumatea/pytorch:1.7.1-py$PYVER-full" -t "kumatea/pytorch:$PYVER-full" .
 else
-    docker build -t "kumatea/pytorch:1.7.1-py$PYVER-full" -t "kumatea/pytorch:$PYVER-full" -f full.Dockerfile .
+    docker build -f full.Dockerfile -t "kumatea/pytorch:1.7.1-py$PYVER-full" -t "kumatea/pytorch:$PYVER-full" .
 fi
