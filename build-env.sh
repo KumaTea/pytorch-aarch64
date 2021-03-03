ff527a755f33d6259a79341f942a6cb6b5b72700
KumaTea <KumaTea@outlook.com>
2021-03-04 02:15:28 +0800

    disable ci

--- build-env.sh
+++ build-env.sh
@@ -3,26 +3,25 @@
 set -xe
 
 cd py39
-docker build --pull -t kumatea/build:py39 -t kumatea/build:latest .
+docker buildx build --pull --push --platform linux/arm64 -t kumatea/build:py39 -t kumatea/build:latest .
 cd ..
 
 cd py38
-docker build --pull -t kumatea/build:py38 .
+docker buildx build --pull --push --platform linux/arm64 -t kumatea/build:py38 .
 cd ..
 
 cd py37
-docker build --pull -t kumatea/build:py37 .
+docker buildx build --pull --push --platform linux/arm64 -t kumatea/build:py37 .
 cd ..
 
 cd py36
-docker build --pull -t kumatea/build:py36 .
+docker buildx build --pull --push --platform linux/arm64 -t kumatea/build:py36 .
 cd ..
 
 cd pypy37
-docker build --pull -t kumatea/build:pypy37 .
+docker buildx build --pull --push --platform linux/arm64 -t kumatea/build:pypy37 .
 cd ..
 
 cd pypy36
-docker build --pull -t kumatea/build:pypy36 .
+docker buildx build --pull --push --platform linux/arm64 -t kumatea/build:pypy36 .
 cd ..
-
