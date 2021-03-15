40be85c1687c719df46a2d372576850b5e0a7948
KumaTea <KumaTea@outlook.com>
2021-03-16 02:02:54 +0800

    torchaudio for aarch64

--- build.bat
+++ build.bat
@@ -1,5 +1,4 @@
 REM docker buildx prune -f
-pause
 
 cd docker
 
@@ -7,22 +6,18 @@ cd py39
 docker buildx build --pull --platform linux/arm64,linux/arm/v7 -t kumatea/pytorch:1.8.0 -t kumatea/pytorch:1.8.0-py39 -t kumatea/pytorch:py39 -t kumatea/pytorch:latest --push .
 docker buildx build --pull --platform linux/arm64,linux/arm/v7 -t kumatea/pytorch:1.8.0-full -t kumatea/pytorch:1.8.0-py39-full -t kumatea/pytorch:py39-full -f full.Dockerfile --push .
 cd ..
-pause
 
 cd py38
 docker buildx build --pull --platform linux/arm64,linux/arm/v7 -t kumatea/pytorch:1.8.0-py38 -t kumatea/pytorch:py38 --push .
 docker buildx build --pull --platform linux/arm64,linux/arm/v7 -t kumatea/pytorch:1.8.0-py38-full -t kumatea/pytorch:py38-full -f full.Dockerfile --push .
 cd ..
-pause
 
-cd py39
+cd py37
 docker buildx build --pull --platform linux/arm64,linux/arm/v7 -t kumatea/pytorch:1.8.0-py37 -t kumatea/pytorch:py37 --push .
 docker buildx build --pull --platform linux/arm64,linux/arm/v7 -t kumatea/pytorch:1.8.0-py37-full -t kumatea/pytorch:py37-full -f full.Dockerfile --push .
 cd ..
-pause
 
-cd py39
+cd py36
 docker buildx build --pull --platform linux/arm64,linux/arm/v7 -t kumatea/pytorch:1.8.0-py36 -t kumatea/pytorch:py36 --push .
 docker buildx build --pull --platform linux/arm64,linux/arm/v7 -t kumatea/pytorch:1.8.0-py36-full -t kumatea/pytorch:py36-full -f full.Dockerfile --push .
 cd ..
-pause
--- docker/py37/full.Dockerfile
+++ docker/py37/full.Dockerfile
@@ -23,14 +23,7 @@ RUN set -ex \
 
 RUN set -ex \
         \
-        && if [ $(uname -m) = 'aarch64' ]; then cp /etc/apt/sources.list /etc/apt/sources.list.bak \
-        && sed -i 's@http://deb.debian.org/debian@https://mirrors.matrix.moe/debian@g' /etc/apt/sources.list \
-        && sed -i 's@http://security.debian.org/debian-security@https://mirrors.matrix.moe/debian-security@g' /etc/apt/sources.list \
-        && apt update \
-        && apt install -y ffmpeg \
-        && rm -rf /var/lib/apt/lists/* \
-        && mv /etc/apt/sources.list.bak /etc/apt/sources.list; else echo 'not aarch64'; fi \
-        && python3 -m pip install -U torchvision torchtext torchcsprng -f https://torch.maku.ml/whl/stable.html -f https://ext.maku.ml/wheels.html \
+        && python3 -m pip install -U $(if [ $(uname -m) = 'aarch64' ]; then echo 'torchaudio'; fi) torchvision==0.9.0 torchtext torchcsprng -f https://torch.maku.ml/whl/stable.html -f https://ext.maku.ml/wheels.html \
         && rm -rf /root/.cache/*
 
 CMD ["python3"]
