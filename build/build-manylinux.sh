28e52a48742a5ec1a78773e0e70b1c696a5db565
KumaTea <KumaTea@outlook.com>
2021-03-27 03:37:54 +0800

    release v1.8.1 related wheels

--- build/build-manylinux.sh
+++ build/build-manylinux.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Build using official manylinux wheels
 
 set -xe
@@ -6,9 +8,20 @@ echo "Building PyTorch"
 
 bash deps.sh
 
-# bash torch.sh "1.8.0" "v1.8.0"
 pip install -U torch -f https://download.pytorch.org/whl/torch_stable.html -f https://ext.maku.ml/wheels.html
+
+cp -a vision vision-bak || :
+export USE_FFMPEG=1
 bash vision.sh "0.9.1" "v0.9.1"
-bash audio.sh "0.8.1" "v0.8.1"
+
+bash audio.sh "0.8.1" "v0.8.1" || bash audio.sh "0.8.1" "v0.8.1"
+# Yes, run twice. I don't know why, but it just works.
+
 bash text.sh "0.9.1" "v0.9.1-rc1"
 bash csprng.sh "0.2.1" "v0.2.1"
+
+# pip uninstall -y torchvision
+# mv vision vision-built
+# mv vision-bak vision || :
+# export USE_FFMPEG=0
+# bash vision.sh "0.9.1+slim" "v0.9.1"
--- test/test.sh
+++ test/test.sh
@@ -2,9 +2,9 @@
 
 set -ex
 
-pip install torch torchvision torchaudio torchtext torchcsprng -f https://torch.maku.ml/whl/stable.html
+pip install torch torchvision==0.9.1 torchaudio torchtext torchcsprng -f https://torch.maku.ml/whl/stable.html
 python3 test/torch-test.py
 
 pip uninstall -y torchvision
-pip install torchvision==0.9.0+slim -f https://torch.maku.ml/whl/stable.html
+pip install torchvision==0.9.1+slim -f https://torch.maku.ml/whl/stable.html
 python3 test/torch-test.py
