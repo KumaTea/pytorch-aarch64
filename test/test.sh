93003e32c4fdbb24b04dc80df1633958776f5ecd
KumaTea <KumaTea@outlook.com>
2021-05-22 20:29:57 +0800

    new wheels

--- test/test.sh
+++ test/test.sh
@@ -2,14 +2,12 @@
 
 set -ex
 
-pip install torch -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch===1.8.1 -f https://torch.kmtea.eu/whl/stable.html
 # aarch64
-
-pip install no-manylinux
 
 pip install torchvision===0.9.1 torchaudio torchtext torchcsprng -f https://torch.kmtea.eu/whl/stable.html
 python3 test/torch-test.py
 
-pip uninstall -y torchvision
-pip install torchvision===0.9.1+slim -f https://torch.kmtea.eu/whl/stable.html
+pip uninstall -y torch torchvision
+pip install torch torchvision -f https://torch.kmtea.eu/whl/stable.html
 python3 test/torch-test.py
