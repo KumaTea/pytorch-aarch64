1e64ae79d3b821f79f157c77572b74a8e01b8422
KumaTea <KumaTea@outlook.com>
2022-03-18 03:15:13 +0800

    Update test-conda.sh

--- test/test-conda.sh
+++ test/test-conda.sh
@@ -2,24 +2,23 @@
 
 set -ex
 
-# sudo apt update
-# sudo apt install -y -qq wget
+PYVER=$1
 
-# bash test/check-arch.sh
+bash test/check-arch.sh
 
 wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-aarch64.sh -O /tmp/install-conda.sh
 bash /tmp/install-conda.sh -b -p /tmp/conda
 
 CONDA_EXE=/tmp/conda/bin/conda
 $CONDA_EXE init bash
-source /home/circleci/.bashrc
+source /home/circleci/.bashrc > /dev/null
 
 # Create a conda environment
 $CONDA_EXE create -n test python=$PYVER -y
-conda activate test
+conda activate test > /dev/null
 
 # Install packages
-PKGS="torchvision"
+PKGS="torchvision>=0.12"
 CONDA_TEST_PYTHON=/tmp/conda/envs/test/bin/python
 
 conda install $PKGS -c kumatea -y
